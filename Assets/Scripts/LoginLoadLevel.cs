230f7377a2e0c88ef3ae87b48ea37aa8428afbdc
Vertti Viitanen <vertti.viitanen@gmail.com>
2016-11-28 17:10:45 +0200

    Unity files

---
+++ Assets/Scripts/LoginLoadLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoginLoadLevel : MonoBehaviour {
+	
+	public InputField usernameInput, passwordInput;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Login()
+	{
+		string username, password;
+		username = usernameInput.text;
+		password = usernameInput.text;
+	}
+}
