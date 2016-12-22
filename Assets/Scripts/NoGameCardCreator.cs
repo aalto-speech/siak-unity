72eeab00a49c2f2c5e116f0263d526334b54282c
Vertti Viitanen <vertti.viitanen@gmail.com>
2016-12-22 23:58:06 +0200

    -NoGame -Placeholder in the 4-6 slot

---
+++ Assets/Scripts/NoGameCardCreator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoGameCardCreator : MonoBehaviour {
+
+    public GameObject card;
+    public float spacing;
+
+    public void CreateCards(string input) {
+        int amount = 1;
+        int.TryParse(input, out amount);
+        float inRow = Mathf.Ceil(Mathf.Sqrt(amount));
+        float inColumn = Mathf.Ceil(amount / inRow);
+        int x = 0;
+        int y = 0;
+        float baseX = -(inRow - 1) / 2;
+        float baseY = (inColumn - 1) / 2;
+
+        for (int i = 0; i < amount; i++) {
+            GameObject go = Instantiate(card);
+            go.transform.position = new Vector3((baseX + x) * spacing, (baseY + y) * spacing, 0);
+            x++;
+            if (x == inRow) {
+                x = 0;
+                y--;
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+}
